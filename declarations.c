7dbe845c06c969957cc970b11fc3cab69d78ffc6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-07-23 22:43:25 +0530

    Add declarations for C

---
+++ declarations.c
@@ -0,0 +1,66 @@
+auto matic;
+auto mechanic;
+
+char acter;
+char coal;
+char izard;
+char mander;
+char meleon;
+
+const able;
+const ant;
+const ipated;
+const ructor;
+
+const int craving;
+
+double tap;
+double trouble;
+
+float away;
+float ing;
+float left;
+float right;
+float yourself;
+
+int eger;
+int elligent;
+int eresting;
+int ern;
+int ernational;
+int roduce;
+
+long ago;
+long day;
+long itude
+long night;
+long story;
+long story, mate;
+long walk;
+
+// -Wimplicit-int
+register here;
+
+short changed;
+short sight;
+short story;
+
+signed artist;
+signed book;
+signed underDuress;
+
+signed long ago;
+
+// -Wimplicit-int
+static electricity;
+static noise;
+
+union dues {};
+
+unsigned artist;
+unsigned autobiography;
+unsigned contract;
+
+// -Wimplicit-int
+volatile liquid;
+volatile substance;
